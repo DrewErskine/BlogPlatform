6aa8c7ee5d2f40f13648aa962d3236698421c70d
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-06 13:00:30 -0400

    Revert "5-5-5 bye bye"
    This reverts commit 4666a16be9e3cc5e0084a674b01a8a5b76d5d7ba.

---
+++ blog/src/test/java/com/UserBlog/Blog/controller/LoginControllerTests.java
@@ -0,0 +1,25 @@
+package com.UserBlog.Blog.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class LoginControllerTests{
+
+    @Mock
+    private Model model;
+
+    @InjectMocks
+    private UserController loginController;
+
+    @Test
+    public void testLogin() {
+        String viewName = loginController.login();
+        assertEquals("login.html", viewName);
+    }
+}
