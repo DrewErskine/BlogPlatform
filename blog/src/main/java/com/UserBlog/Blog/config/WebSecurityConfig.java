bfc8655c434a5d9b5f92ea123d7a1c2fc324d90e
dmerskine19 <81436471+dmerskine19@users.noreply.github.com>
2024-05-09 16:23:51 -0400

    PeepPplog

--- blog/src/main/java/com/UserBlog/Blog/config/WebSecurityConfig.java
+++ blog/src/main/java/com/UserBlog/Blog/config/WebSecurityConfig.java
@@ -1,11 +1,15 @@
 package com.UserBlog.Blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -13,6 +17,12 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig{
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,10 +48,13 @@ public class WebSecurityConfig{
             .headers(headers -> headers
                 .frameOptions(frame -> frame.deny())
                 .contentTypeOptions(contentType -> contentType.disable())
-                .contentSecurityPolicy(csp -> csp.policyDirectives("default-src 'self'")))
-            .rememberMe(rememberMe -> rememberMe
-                .tokenValiditySeconds(86400)
-                .key("uniqueAndSecret"));
+                .contentSecurityPolicy(csp -> csp.policyDirectives("default-src 'self'"))
+            );
         return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 }
